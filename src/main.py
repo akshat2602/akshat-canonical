8d27b1290a126dde47b2cb942c8b5bf8e522fc3c
Akshat Sharma <akshatsharma2602@gmail.com>
2024-09-24 13:09:26 -0400

    feat: mvp apis with hardcoded data and file upload validation

--- src/main.py
+++ src/main.py
@@ -1,15 +1,63 @@
-from typing import Union
-
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, status, Response
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get("/")
+class FileUploadSuccessMessage(BaseModel):
+    message: str
+
+
+class ErrorMessage(BaseModel):
+    error: str
+
+
+class GetReportResponse(BaseModel):
+    gross_revenue: float
+    expense: float
+    net_revenue: float
+
+
+@app.get("/ping")
 def read_root():
-    return {"Hello": "World"}
+    return "pong"
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/report/", response_model=GetReportResponse)
+async def get_report():
+    return {
+        "gross_revenue": 1000.0,
+        "expense": 500.0,
+        "net_revenue": 500.0,
+    }
+
+
+@app.post(
+    "/transactions/",
+    responses={
+        200: {
+            "model": FileUploadSuccessMessage,
+            "description": "CSV received and processed",
+        },
+        400: {"model": ErrorMessage, "description": "Bad file uploaded"},
+    },
+)
+async def create_upload_file(file: UploadFile, response: Response):
+    """
+    This endpoint is used to upload a CSV file to the server.
+    This file should be formatted as a CSV file with the following columns:
+    `Date, Type, Amount($), Memo`
+    """
+    # Read the file and validate that it is a CSV file
+    if file.content_type != "text/csv":
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return {
+            "error": "The file must be a CSV file",
+        }
+
+    # Read the file and print first 10 lines
+    contents = await file.read()
+    lines = contents.decode().split("\n")
+    print(lines)
+
+    return FileUploadSuccessMessage(message="CSV received and processed")
